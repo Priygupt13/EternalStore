e447e5891f08ed02399e4aaa478836ce6cffc549
Priya Gupta <er.priyagupta123@gmail.com>
2022-10-10 01:04:11 -0700

    Implement create/delete controllers

--- backend/controllers/file.controller.js
+++ backend/controllers/file.controller.js
@@ -1,10 +1,21 @@
+const uuid = require("uuid");
+const { deleteLocalFile } = require("../util/file.local")
+const uploadFile = require("../middleware/localUpload");
 const db = require("../models");
 const File = db.file;
+
+
+const  getFileUpdateTimestamp = (item) => {
+    return Math.floor(
+        new Date(item.updatedAt).getTime()/1000);
+}
 
 const getPublicFileMetadata = (item) => {
     return {
         id: item.id,
-        name: item.name
+        name: item.name,
+        url: item.url,
+        update_timestamp: getFileUpdateTimestamp(item)
     };
 }
 
@@ -12,6 +23,8 @@ const getPublicFileMetadata = (item) => {
 exports.getFilesForAdmin = (req, res) => {
     File.findAll().then(
         result => {res.status(200).send(result.map(getPublicFileMetadata));}
+    ).catch(
+        res.status(404).send({error: "Failed to retrieve files."})
     );
 };
 
@@ -26,22 +39,108 @@ exports.getFiles = (req, res) => {
 
 // download files
 exports.downloadFile = (req, res) => {
+    const fileId = req.params.id;
+    // Get the id
+    // Return url
     res.status(200).send("File downloaded successfully.");
 };
 
 // Create a new file.
 // A duplicate file is also treated a new file.
-exports.createFile = (req, res) => {
-    res.status(200).send("File uploaded successfully.");
+exports.createFile = async (req, res) => {
+    const fileId = uuid.v4();
+    req.fileId = fileId;
+
+    try {
+        // Upload the file
+        await uploadFile(req, res);
+    
+        if (req.file == undefined) {
+          return res.status(400).send({ error: "Please upload a file!" });
+        }
+
+        // Update the DB
+        File.create({
+            id: fileId,
+            name: req.file.originalname,
+            url: req.file.path,
+            userId: req.userId
+        }).then(res.status(200).send({
+            message: "Uploaded the file successfully: " + req.file.originalname,
+        }));
+      } catch (err) {
+        console.log(err);
+    
+        if (err.code == "LIMIT_FILE_SIZE") {
+          return res.status(500).send({
+            message: "File size cannot be larger than 2MB!",
+          });
+        }
+    
+        res.status(500).send({
+          message: `Could not upload the file: ${req.file.originalname}. ${err}`,
+        });
+      }
 };
 
 // Updates an existing file.
 // If the file doesn't exist then creates a new file.
-exports.updateFile = (req, res) => {
-    res.status(200).send("File updated successfully.");
+exports.updateFile = async (req, res) => {
+    const fileId = req.params.id;
+    req.fileId = fileId;
+
+    // Ensure file exists
+    File.findOne({
+        where: { id: fileId }
+    }).catch(err => res.status(404).send({error: "File doesn't exist,"}));
+
+    // Update file.
+    try {
+        // Upload the file
+        await uploadFile(req, res);
+    
+        if (req.file == undefined) {
+          return res.status(400).send({ error: "Please upload a file!" });
+        }
+
+        // Update the DB
+        File.update({
+            id: fileId,
+            url: req.file.path
+        }).then(res.status(200).send({
+            message: "Updated the file successfully: " + req.file.originalname,
+        }));
+      } catch (err) {
+        console.log(err);
+    
+        if (err.code == "LIMIT_FILE_SIZE") {
+          return res.status(500).send({
+            message: "File size cannot be larger than 2MB!",
+          });
+        }
+    
+        res.status(500).send({
+          message: `Could not upload the file: ${req.file.originalname}. ${err}`,
+        });
+      }
 };
 
 // Deletes the file.
 exports.deleteFile = (req, res) => {
-    res.status(200).send("File deleted successfully.");
+    const fileId = req.params.id;
+    File.findOne({
+        where: { id: fileId }
+    }).then(
+        result => {
+            const fileUrl = result.url;
+            deleteLocalFile(fileUrl);
+    }).then(
+        File.destroy({where: {id: fileId}})
+    ).then(
+        res.status(200).send("File deleted successfully.")
+    ).catch(
+        error => {
+            res.status(404).send({ error: "Failed to delete file. Err: "  + error });
+        }
+    );
 };
