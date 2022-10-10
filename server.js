6e87e71ebc1c1e4f7a9799313d7c32add62984a0
Priya Gupta <er.priyagupta123@gmail.com>
2022-10-09 18:00:25 -0700

    Setup mysql db tables for auth

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+const cors = require("cors");
+const db = require("./backend/models");
 var path = require('path');
 
 var publicDir = path.join(__dirname,'public');
@@ -6,14 +8,25 @@ var publicDir = path.join(__dirname,'public');
 class Server {
     constructor() {
         this.app = express();
-        this.port = process.env.PORT || 3000;
+        this.port = process.env.PORT || 5000;
         this.frontend_dir = path.join(__dirname,'./frontend/build');
+        this.corsOptions = { origin: "http://localhost:3000" };
+
+        // Setup DB
+        this.role = db.role;
+        db.sequelize.sync({force: true}).then(() => {
+            console.log('Drop and Resync Database with { force: true }');
+            this.initialize_db();
+        });
 
         this.middlewares();
         this.routes();
     }
 
     middlewares() {
+        this.app.use(cors(this.corsOptions));
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: true }))
         this.app.use(express.static(this.frontend_dir));
     }
 
@@ -30,6 +43,18 @@ class Server {
         this.app.listen(this.port, () => {
             console.log("Server running on port: ", this.port);
           });
+    }
+
+    initialize_db() {
+        this.role.create({
+            id: 1,
+            name: "user"
+        });
+         
+        this.role.create({
+            id: 2,
+            name: "admin"
+        });
     }
 }
 
