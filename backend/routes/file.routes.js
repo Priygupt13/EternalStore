1f935363443e6a41be6cbb7b95e4a8602669e9a6
Priya Gupta <er.priyagupta123@gmail.com>
2022-10-09 21:08:26 -0700

    Pass file id in url for download/update/delete operation

--- backend/routes/file.routes.js
+++ backend/routes/file.routes.js
@@ -12,10 +12,10 @@ module.exports = function(app) {
 
   app.get("/api/file/admin/list", [authJwt.verifyToken, authJwt.isAdmin], controller.getFilesForAdmin);
   app.get("/api/file/list", [authJwt.verifyToken], controller.getFiles);
-  app.get("/api/file/download", [authJwt.verifyToken], controller.downloadFile);
+  app.get("/api/file/:id", [authJwt.verifyToken], controller.downloadFile);
 
   app.post("/api/file/create", [authJwt.verifyToken], controller.createFile);
-  app.post("/api/file/update", [authJwt.verifyToken], controller.updateFile);
+  app.post("/api/file/update/:id", [authJwt.verifyToken], controller.updateFile);
   
-  app.delete("/api/file/delete", [authJwt.verifyToken], controller.deleteFile);
+  app.delete("/api/file/:id", [authJwt.verifyToken], controller.deleteFile);
 };
