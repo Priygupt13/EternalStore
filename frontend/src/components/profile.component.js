50eb4c9336bc1e6f9e4e0b578de81009969fff8d
Priya Gupta <er.priyagupta123@gmail.com>
2022-10-10 19:28:58 -0700

    Profile page should show User name but not auth token

--- frontend/src/components/profile.component.js
+++ frontend/src/components/profile.component.js
@@ -37,13 +37,8 @@ class Profile extends Component {
           </h3>
         </header>
         <p>
-          <strong>Token:</strong>{" "}
-          {currentUser.accessToken.substring(0, 20)} ...{" "}
-          {currentUser.accessToken.substr(currentUser.accessToken.length - 20)}
-        </p>
-        <p>
-          <strong>Id:</strong>{" "}
-          {currentUser.id}
+          <strong>Name:</strong>{" "}
+          {currentUser.firstName + " " + currentUser.lastName}
         </p>
         <p>
           <strong>Email:</strong>{" "}
